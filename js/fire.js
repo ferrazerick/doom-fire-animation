4253146391ecbf444c0b5c5cbed58b13eba3840a
ferrazerick <erickferraz2018@gmail.com>
2026-01-31 22:34:05 -0300

    Creating table with some values to see result on screen

--- js/fire.js
+++ js/fire.js
@@ -1,11 +1,12 @@
 const firePixelsArray = []
-const fireWidth = 2
-const fireHeight = 3
+const fireWidth = 10
+const fireHeight = 10
 
 
 function start() {
     createFireDataStructure()
-    console.log(firePixelsArray)
+    createFireSource()
+    renderFire()
 }
 
 function createFireDataStructure() {
@@ -17,13 +18,46 @@ function createFireDataStructure() {
 }
 
 function calculateFirePropagation() {
+    const numberOfPixels = fireWidth * fireHeight
+
+    for (let column = 0; column < numberOfPixels; column++) {
+        for (let row = 0; row < fireHeight; row++) {
+            const pixelIndex = column + fireWidth * row
+        }
+    }
 }   
 
 function renderFire() {
     let html = '<table cellpadding=0 cellspancing=0>'
 
-    for (let row = 0; row < fireHeight; row++)
-    html += '<table>'
+    for (let row = 0; row < fireHeight; row++) {
+        html += '<tr>'
+
+        for (let column = 0; column < fireWidth; column++) {
+            const pixelIndex = column + (fireWidth * row)
+            const fireIntensity = firePixelsArray[pixelIndex]
+
+            html += '<td>'
+            html += `<div class="pixel-index">${pixelIndex}</div>`
+            html += fireIntensity
+            html += '</td>'
+        }
+
+        html += '</tr>'
+    }
+
+    html += '</table>'
+
+    document.querySelector('#fireCanvas').innerHTML = html
+}
+
+function createFireSource() {
+    for (let column = 0; column <= fireWidth; column++) {
+        const overflowPixelIndex = fireWidth * fireHeight
+        const pixelIndex = (overflowPixelIndex - fireWidth) + column
+
+        firePixelsArray[pixelIndex] = 36
+    }
 }
 
 start()
